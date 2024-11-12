d8c630376da7a3351bd45e2382d95b73b4bed723
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-11-12 16:22:22 +0530

    96. Delete: delete articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] #destroy is route for the delete.
+  # Above code statement have same meaning as below code statement.
+  resources :articles
 end
