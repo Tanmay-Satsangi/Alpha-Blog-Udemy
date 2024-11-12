e99d2cef2ab3799773b2341ccf775969d5de7382
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-11-12 17:28:39 +0530

    pages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] #destroy is route for the delete.
   # Above code statement have same meaning as below code statement.
   resources :articles
