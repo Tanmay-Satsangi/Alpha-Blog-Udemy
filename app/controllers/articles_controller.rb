0c01c34a462e48d6e5b7836ab7c211eb0e4819a9
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-11-22 15:22:31 +0530

    130. One to many association

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,8 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
+    #Temporary hardcode the user-id so, we can create the articles from front end.
+    @article.user_id = User.first.id
     
     if @article.save
       flash[:notice] = "Article was created successfully"
