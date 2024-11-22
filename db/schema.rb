2205704c72ed57d0478ff334b7ec442e9a4ab156
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-11-22 11:47:30 +0530

    127. Create users - text directions and code

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_05_153049) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_20_105740) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,12 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_153049) do
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 end
