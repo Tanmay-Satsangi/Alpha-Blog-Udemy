2f3ce188af7eb00ca01357ccc2b2cdf530592b82
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-11-23 14:43:01 +0530

    138. New user signup form

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
   # resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy] #destroy is route for the delete.
   # Above code statement have same meaning as below code statement.
   resources :articles
+  get 'signup', to: "users#new"
+
+  resources :users, except: [:new]
 end
