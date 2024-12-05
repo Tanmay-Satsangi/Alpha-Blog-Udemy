b1e4f5b60701ff8c992eadb9547f6b40f18eb8ae
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-12-05 15:10:37 +0530

    156. Authentication helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
 
     image_tag(gravatar_url, alt: user.username)
   end
+
+  def current_user
+    # Here memoization is used. 
+    # Note: Here "session[:user_id]" is same as "sessions_controller", "session[:user_id]"
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user
+  end
 end
