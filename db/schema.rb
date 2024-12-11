b6681bd3b6221c1ef622bc02c430083d86afd5dd
Tanmay-Satsangi <72879992+Tanmay-Satsangi@users.noreply.github.com>
2024-12-11 23:18:41 +0530

    170. Add admin user functionality - intro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_22_114535) do
+ActiveRecord::Schema[7.2].define(version: 2024_12_11_105649) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -28,5 +28,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_22_114535) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 end
